171b2fe0d762eab9b42c01635c68137379900516
ADITYA SHUKLA <adityashukla74@gmail.com>
2016-08-13 14:32:44 +0530

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813084559) do
+ActiveRecord::Schema.define(version: 20160813085640) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "date"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
