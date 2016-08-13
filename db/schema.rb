84a24609dfaf90508695e6ea62182ce38c8cc594
ADITYA SHUKLA <adityashukla74@gmail.com>
2016-08-13 20:03:06 +0530

    migrated, indenting in gemfile and created a bit layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813085640) do
+ActiveRecord::Schema.define(version: 20160813132514) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "date"
-    t.integer  "location_id"
   end
 
   create_table "locations", force: :cascade do |t|
-    t.string   "name"
-    t.text     "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -40,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160813085640) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
