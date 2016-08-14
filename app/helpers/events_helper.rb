1aa817406201e104d576ffe10e7df575b3ed4aa8
ADITYA SHUKLA <adityashukla74@gmail.com>
2016-08-14 14:28:32 +0530

    routes for attending event and format

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,5 @@
 module EventsHelper
-	def check_volunteer_status (event_id)
+	def check_attend_status (event_id)
 	    @rsvp = UserRsvp.where(:user_id => current_user, :event_id => event_id).first
 	    return @rsvp.nil?
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   root :to => "events#index"
+  
   devise_scope :user do 
     match "/users/sign_up", :to => "devise/registrations#new", via: :get
   end 
+
+  match 'events/attend/:id', :to => 'events#attend', :as => :attend
 end
