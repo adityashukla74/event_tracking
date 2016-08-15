e63fd16837dab994ed61cef96da8be4583169781
ADITYA SHUKLA <adityashukla74@gmail.com>
2016-08-15 18:58:15 +0530

    migration to add index to attendrsvp

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,2 @@
 module EventsHelper
-	def check_attend_status (event_id)
-	    @rsvp = AttendRsvp.where(:user_id => current_user, :event_id => event_id).first
-	    if @rsvp.nil?
-      		return true
-    	end
-    	return not(@rsvp.attending)
-  	end
 end
---
+++ db/migrate/20160815132632_add_index_to_attend_rsvp.rb
@@ -0,0 +1,4 @@
+class AddIndexToAttendRsvp < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
