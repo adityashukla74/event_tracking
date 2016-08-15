ea752107f5be3ccf7e4fd1010cc1e404e63b96bf
ADITYA SHUKLA <adityashukla74@gmail.com>
2016-08-15 18:43:07 +0530

    validations for Event title and location

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
 	belongs_to :location
 	has_many :attendRsvps
- +  has_many :users, :through => :attendRsvps
+    has_many :users, :through => :attendRsvps
+ 	validates_presence_of :title
+    validates_presence_of :location
+    validates_presence_of :date
 end
